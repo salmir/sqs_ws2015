807e000e32fcb35f63d6ca3a9f91a169fd308805
Salmir Delalic <delalic.salmir@gmail.com>
2015-12-16 12:35:15 +0100

    Triangle Klasse erstellt (Methodenköpfe und Variablen)

--- 2 Test Driven Development/QSVU_UEbung3_0947919_Delalic_Salmir_Triangle.java
+++ 2 Test Driven Development/QSVU_UEbung3_0947919_Delalic_Salmir_Triangle.java
@@ -1,5 +1,49 @@
 
 public class QSVU_UEbung3_0947919_Delalic_Salmir_Triangle {
 
-    return;
+    private double a, b;
+    private double hyp; //hyp.. Hypothenuse
+
+    /**
+     * Calculates the hypothenuse of the triangle by knowing the other two lenghts
+     */
+    private void calcHyp() {
+
+    }
+
+    /**
+     * Calculates the 3rd angle of the triangle, with a second known angle as input
+     * @param knownAngle not the right Angle! (less then 90)
+     */
+    private void calcAngle(double knownAngle) {
+
+    }
+
+    /**
+     * Calculates the surface of the triangle
+     * @return the surface of the triangle
+     */
+    private double calcArea() {
+
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getHyp {
+        return hyp;
+    }
 }
